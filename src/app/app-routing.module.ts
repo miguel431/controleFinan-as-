5141c90132fb0ea3ae23a6a43a4f3bfd758e0308
miguel431 <miguelnatdk@gmail.com>
2024-10-28 20:00:04 -0300

    página de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:LoginComponent
+  },
+
+  {
+    path:'login', component:LoginComponent
+  },
+
+  {
+    path:'cadastro', component:CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
